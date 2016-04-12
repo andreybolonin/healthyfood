dc37342005ef722389b1f475a3c931aa04e01f48
root <root@43637.simplecloud.club>
2016-04-12 04:16:36 +0000

    added blog

--- blog/vendor/autoload.php
+++ blog/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitc351bae8d0981d0f42b2d063298ae9db::getLoader();
+return ComposerAutoloaderInitc23e92b93b8ae115ab01441015c5c62f::getLoader();

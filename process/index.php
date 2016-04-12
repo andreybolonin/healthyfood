ffc7a4c881e6aa35ed2050680203e48aada39d44
andreybolonin <andreybolonin1989@gmail.com>
2016-04-12 12:21:56 +0300

    fixed payment

--- process/index.php
+++ process/index.php
@@ -9,21 +9,34 @@ $app->get('/', function() use($app) {
 
     $public_key = 'i21026092163';
     $private_key = 'Lfj88TBWC2wCc9T8vCm2ZunA5qBKkR8SZAKcN0h0';
-
     $liqpay = new LiqPay($public_key, $private_key);
-    $response = $liqpay->api("payment/pay", array(
-        'version'        => '3',
-        'phone'          => '380950000001',
-        'amount'         => '1',
-        'currency'       => 'USD',
-        'description'    => 'description text',
-        'order_id'       => 'order_id_1',
-        'card'           => '4731195301524634',
-        'card_exp_month' => '03',
-        'card_exp_year'  => '22',
-        'card_cvv'       => '111',
-        'sandbox'        => true
+
+    $html = $liqpay->cnb_form(array(
+        'version' => 3,
+        'public_key' => 'i21026092163',
+        'action' => 'subscribe',
+        'amount' => '1',
+        'currency' => 'UAH',
+        'description' => 'test description',
+        'order_id' => 'test order_id',
+        'sandbox' => 1
     ));
+    
+    return $html;
+
+    $json_string = [
+        'version' => 3,
+        'public_key' => 'i21026092163',
+        'action' => 'subscribe',
+        'amount' => '1',
+        'currency' => 'UAH',
+        'description' => 'test description',
+        'order_id' => 'test order_id'
+    ];
+
+    $data = base64_encode($json_string);
+
+    $signature = base64_encode(sha1($private_key.$data.$private_key, 1));
 
     var_dump($response);exit;
     return 'Hello '.$app->escape($res);
